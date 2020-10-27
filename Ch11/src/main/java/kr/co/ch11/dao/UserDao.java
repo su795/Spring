842ac28ba1bc1f2dd8100ff824c6a51ead97e198
ligr00vefe <ligr00vefe@naver.com>
2020-10-27 13:57:31 +0900

    2020/09/18 Spring 수업 Spring-Boot 데이터베이스

---
+++ Ch11/src/main/java/kr/co/ch11/dao/UserDao.java
@@ -0,0 +1,19 @@
+package kr.co.ch11.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import kr.co.ch11.vo.UserVO;
+
+@Repository
+public interface UserDao {
+	
+	public void insertUser(UserVO vo);
+	public UserVO selectUser();
+	public List<UserVO> selectUsers();
+	public void updateUser(UserVO vo);
+	public void deleteUser(String uid);
+	
+}
---
+++ Ch11/src/main/java/kr/co/ch11/service/UserService.java
@@ -0,0 +1,39 @@
+package kr.co.ch11.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.ch11.dao.UserDao;
+import kr.co.ch11.vo.UserVO;
+
+@Service
+public class UserService {
+
+	// Springboot에서는  inject를 쓸 수 없음
+	@Autowired
+	private UserDao dao;
+	
+	public void insertUser(UserVO vo) {
+		dao.insertUser(vo);
+	};
+	
+	public UserVO selectUser() {
+		return dao.selectUser();
+	};
+	
+	public List<UserVO> selectUsers() {
+		return dao.selectUsers();
+	};
+	
+	public void updateUser(UserVO vo) {
+		dao.updateUser(vo);
+	};
+	
+	public void deleteUser(String uid) {
+		dao.deleteUser(uid);
+	};
+	
+	
+}
